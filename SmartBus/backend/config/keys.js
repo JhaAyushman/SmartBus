d1f4408cab83aa2d585f536446956bb9ca9ca59d
JhaAyushman <jhaayushman2004@gmail.com>
2025-11-11 18:54:28 +0530

    up

--- SmartBus/backend/config/keys.js
+++ SmartBus/backend/config/keys.js
@@ -1,3 +1,3 @@
-export const MongoURI = "mongodb+srv://ayushman:Ayushman10@cluster0.zcyyvnf.mongodb.net/";
+export const MongoURI = "mongodb+srv://ayushman:Ayushman10@cluster0.zcyyvnf.mongodb.net/?appName=Cluster0";
 export const JWT_SECRET = "top_secret";
 export const FRONTEND_URL="https://smartbus-s64j.onrender.com";
